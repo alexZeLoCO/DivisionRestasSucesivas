30db1fbdcd1f90d111d30f8ef47829b095548717
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-10-29 12:03:46 +0100

    Segundo Commit

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ divisor=int(input("Introduzca un dividendo: "))     #SOLICITA VALOR
 resto=dividendo     #RESTO ES DIVIDENDO
 sol=0       #SOLUCIÓN
 
-while resto>=divisor:       #MIENTRAS EL RESTO SEA MAYOR O IGUAL AL DIVISOR
-    resto=resto-divisor     #EL RESTO ES EL DIVIDENDO MENOS EL DIVISOR
-    sol=sol+1       #COCIENTE
+if divisor==0:      #DIVISOR 0
+    print("El divisor es 0. No existe solución.")       #OUTPUT
+else:
+    while resto>=divisor:       #MIENTRAS EL RESTO SEA MAYOR O IGUAL AL DIVISOR
+        resto=resto-divisor     #EL RESTO ES EL DIVIDENDO MENOS EL DIVISOR
+        sol=sol+1       #COCIENTE
 
-print ("El cociente de la división es:",sol)       #OUTPUT
+    print ("El cociente de la división es:",sol)       #OUTPUT
